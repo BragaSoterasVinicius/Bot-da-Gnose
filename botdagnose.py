ae00ca6f9970e56da0e6860740289f4c9f7cca3e
BragaSoterasVinicius <viniciussoteras@gmail.com>
2024-07-10 23:31:35 -0300

    gnose intensificada
    um terço das mensagems com mais de 10 palavras será gnose

--- botdagnose.py
+++ botdagnose.py
@@ -1,24 +1,32 @@
 from random import randrange as r
+from math import floor 
 
 def gnose_maker(originalString):
-    print("or" + originalString)
+    #Adicionar um conversor de lista para string aqui, vai melhorar a orga-
+    #nização pra quando rodar esse arquivo isolado em testes.
     if isinstance(originalString,str):
-        if len(originalString.split()) > 3: 
-            randomWord = r(len(originalString.split()))
-            finalString = originalString.replace(originalString.split(" ")[randomWord], "gnose")
-            print(finalString)
+        print("or" + originalString)
+        textSize = len(originalString.split())
+        if textSize > 3:
+            if textSize > 10: #retirar linha no futuro?
+                for n in range(floor(10/3)):
+                    randomWord = r(len(originalString.split()))
+                    originalString = originalString.replace(originalString.split(" ")[randomWord], "gnose")
+                    print(originalString)
+        randomWord = r(len(originalString.split()))
+        finalString = originalString.replace(originalString.split(" ")[randomWord], "gnose")
+        print(finalString)
     else:
         randomWord = r(len(originalString))
         originalString[randomWord] = "gnose"
         finalString = " ".join(map(str,originalString))
         print(finalString)
     return finalString
-    return None
 
 if __name__ == "__main__":
     import sys
     print("run")
     sys.argv = sys.argv[1:]
-    gnose_maker("bom dia")
+    gnose_maker(sys.argv)
 
 
